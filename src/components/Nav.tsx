4eb46000d0a5de21ab0c2ee9672738fc943f282e
melsafi1 <56850296+melsafi1@users.noreply.github.com>
2023-11-22 22:38:30 -0800

    removed assets in src

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,6 @@
-import logo from "../assets/logo.svg";
 import { Toggle } from "../components/themeToggle";
 import * as React from "react";
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
-import styled from "styled-components";
 import { LinkStyle } from "../components/StyledButton";
 import Image from "next/image";
 //import { animateScroll as scroll } from "react-scroll";
@@ -11,56 +9,6 @@ type NavProps = {
   checked: boolean;
   onChange: any;
 };
-
-const NavContainer = styled.div<{ checked: boolean }>`
-  height: 100px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  width: 100vw;
-  z-index: 100;
-  background-image: ${(props) =>
-    props.checked
-      ? `linear-gradient(180deg, #0C0C0F, rgba(12, 12, 15, 0.8) 59%, transparent);`
-      : `linear-gradient(180deg, #F5F5F5, rgba(245, 245, 245, 0.8) 59%, transparent);`};
-`;
-
-const LeftGroup = styled.div`
-  display: flex;
-  flex-basis: 20%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-`;
-
-const LogoButton = styled.div<{ checked: boolean }>`
-  filter: ${(props) => (props.checked ? "invert(0%)" : "invert(100%)")};
-  position: relative;
-  width: 50px;
-  height: 50px;
-`;
-const RightGroup = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  margin-right: 2rem;
-  flex-basis: 10%;
-  height: 100%;
-`;
-
-const MiddleGroup = styled.div`
-  display: flex;
-  justify-content: Center;
-  align-items: center;
-  flex-basis: 70%;
-  height: 100%;
-  gap: 2rem;
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
 
 const Nav: React.FunctionComponent<NavProps> = ({ onChange, checked }) => {
   const { scrollY } = useScroll();
